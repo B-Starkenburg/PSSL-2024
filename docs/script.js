618afa583df121ec5de8916f46541a108c77baad
Berend Van Starkenburg <berendvanstarkenburg@Berends-MacBook-Air.local>
2024-08-23 17:44:38 +0200

    mobile styling done

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,16 @@
 function toggleMenu() {
     var mobsidebar = document.querySelector('.mobile-sidebar');
+    var menubar = document.querySelector('.menu-icon');
+
     mobsidebar.classList.toggle('active');
+
+    if (menubar.textContent != '×') {
+        menubar.textContent = '×';
+    }
+
+    else {
+        menubar.textContent = '☰ PSSL 109';
+    }
 }
 
 window.onscroll = function() {toggleNavbarOpacity()};
@@ -12,8 +22,10 @@ function toggleNavbarOpacity() {
     if (window.pageYOffset > 50) { // Adjust the scroll distance as needed
         navbar.style.backgroundColor = "#333"; // Opaque background when scrolled
         sidebar.style.backgroundColor = "#444"; // Opaque background for sidebar
+        navbar.style.color = "#b8860b";
     } else {
         navbar.style.backgroundColor = "transparent"; // Transparent background at the top
         sidebar.style.backgroundColor = "transparent"; // Transparent sidebar at the top
+        navbar.style.color = "#fff";
     }
 }
