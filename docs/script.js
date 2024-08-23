1166ce8b45cd9cb36c797bb67e689f8022316699
Berend Van Starkenburg <berendvanstarkenburg@Berends-MacBook-Air.local>
2024-08-23 16:02:45 +0200

    sidebar tweaking

--- docs/script.js
+++ docs/script.js
@@ -2,3 +2,18 @@ function toggleMenu() {
     var sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('active');
 }
+
+window.onscroll = function() {toggleNavbarOpacity()};
+
+function toggleNavbarOpacity() {
+    var navbar = document.querySelector('.navbar');
+    var sidebar = document.querySelector('.sidebar');
+    
+    if (window.pageYOffset > 50) { // Adjust the scroll distance as needed
+        navbar.style.backgroundColor = "#333"; // Opaque background when scrolled
+        sidebar.style.backgroundColor = "#444"; // Opaque background for sidebar
+    } else {
+        navbar.style.backgroundColor = "transparent"; // Transparent background at the top
+        sidebar.style.backgroundColor = "transparent"; // Transparent sidebar at the top
+    }
+}
