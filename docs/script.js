76d8ece240b7e61c3a52f0cb339ef37168347d22
Berend Van Starkenburg <berendvanstarkenburg@Berends-MacBook-Air.local>
2024-08-23 15:32:02 +0200

    changed something

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,8 @@ function toggleMenu() {
     var sidebar = document.querySelector('.sidebar');
     if (sidebar.style.display === 'block') {
         sidebar.style.display = 'none';
-    } else {
+    } 
+    if (sidebar.style.display === 'none') {
         sidebar.style.display = 'block';
     }
 }
